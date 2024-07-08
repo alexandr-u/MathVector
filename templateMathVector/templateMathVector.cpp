04f2a293f8320c52200b24a3042d182d9d3869b5
Alexandra Uvarova <2210asd2210@gmail.com>
2024-07-08 11:25:06 +0300

    corrected version

--- templateMathVector/templateMathVector.cpp
+++ templateMathVector/templateMathVector.cpp
@@ -1,6 +1,26 @@
 ﻿#include <iostream>
 #include <windows.h>
 #include <math.h>
+template <typename T>
+class BaseMathVectors {
+public:
+    T xVectorX;
+    T yVectorX;
+    T xVectorY;
+    T yVectorY;
+
+    friend std::istream& operator>>(std::istream& is, BaseMathVectors& item) //перегрузка метода ввода для вектора
+    {
+        is >> item.xVectorX >> item.yVectorX >> item.xVectorY >> item.yVectorY;
+        return is;
+    }
+
+    friend std::ostream& operator<<(std::ostream& os, BaseMathVectors& item) //перегрузка метода вывода для вектора
+    {
+        os << "\nКоордината x базового вектора на ось x = " << item.xVectorX << " Координата y базового вектора на ось x = " << item.yVectorX << "\nКоордината x базового вектора на ось y = " << item.xVectorY << " Координата y базового вектора на ось y = " << item.yVectorY << "\n";
+        return os;
+    }
+};
 
 template <typename T>
 
@@ -17,21 +37,22 @@ public:
         x = valueX;
         y = valueY;
     }
-    MathVector(const MathVector& over) //конструктор присваивания
-    {
-        this->x = over.x;
-        this->y = over.y;
-    }
+    ~MathVector() = default; //деструктор по умолчанию
+
+    MathVector(const MathVector& over) = default;
 
     T getX() { return x; }
     T getY() { return y; }
     void setX(T valueX) { x = valueX; }
     void setY(T valueY) { y = valueY; }
 
-    MathVector& operator = (const MathVector& over) //перегрузка оператора присваивания
+    MathVector& operator = (const MathVector& over) //перегрузка оператора =
     {
-        this->x = over.x;
-        this->y = over.y;
+        if (this != &over)
+        {
+            this->x = over.x;
+            this->y = over.y;
+        }
         return *this;
     }
 
@@ -42,6 +63,8 @@ public:
         temp.y = this->y + over.y;
         return temp;
     }
+    MathVector operator += (const MathVector& over) { return *this = *this + over; }
+
 
     MathVector operator - (const MathVector& over) //вычитание векторов
     {
@@ -50,6 +73,8 @@ public:
         temp.y = this->y - over.y;
         return temp;
     }
+    MathVector operator -= (const MathVector& over) { return *this = *this - over; }
+
 
     MathVector operator * (const T& number) //умножение на число
     {
@@ -58,37 +83,33 @@ public:
         temp.y = this->y * number;
         return temp;
     }
+    MathVector operator *= (const double& number) { return *this = *this * number; }
 
     T operator * (const MathVector& over) //скалярное произведение векторов
     {
         return (this->x * over.x + this->y * over.y);
     }
 
-    double longVector() //длина верктора
+    double length() //длина верктора
     {
         return sqrt(double(pow(x, 2) + pow(y, 2)));
     }
 
     MathVector& normal(const MathVector& over) //нормализация вектора (приведение к единичной длине)
     {
-        this->x = double(over.x) / this->longVector();
-        this->y = double(over.y) / this->longVector();
+        this->x = double(this->x) / this->length();
+        this->y = double(this->y) / this->length();
         return *this;
     }
 
-    void base(MathVector& baseVector_x, MathVector& baseVector_y) //разложение на базовые вектора
+    BaseMathVectors<T> base() //разложение на базовые вектора
     {
-        baseVector_x.x = this->x;
-        baseVector_x.y = 0;
-        baseVector_y.x = 0;
-        baseVector_y.y = this->y;
-    }
-
-    MathVector intersection() //пересечение векторов 
-    {
-        MathVector temp;
-        // на подумать. как пересекаются вектора. пересекаются только отрезки. либо нужно две точки для вектора
-        return temp;
+        BaseMathVectors<T> baseVectors;
+        baseVectors.xVectorX = this->x;
+        baseVectors.yVectorX = 0;
+        baseVectors.xVectorY = 0;
+        baseVectors.yVectorY = this->y;
+        return baseVectors;
     }
     friend std::istream& operator>>(std::istream& is, MathVector& item) //перегрузка метода ввода для вектора
     {
@@ -101,8 +122,6 @@ public:
         os << "\n x = " << item.x << " y = " << item.y << "\n";
         return os;
     }
-
-    ~MathVector() = default; //базовый деструктор
 };
 
 
@@ -112,12 +131,24 @@ int main()
 
     MathVector<double>  a(1, 2.3);
     MathVector<double>  b(a);
-    MathVector<double>  c = a + b;
+    /*MathVector<double>  c = a + b;
     MathVector<double>  d = a - c;
     MathVector<double>  e = a * 5;
     double f = a * c;
     std::cout << "Создание вектра с инициализайией значений a(1, 2.3) " << a << "Инициализация с приравниванием к а b(a) " << b << "Вектор с = a+b " << c << "Вектора a-c " << d << "Умножение вектора a на 5" << e << "Скалярное произведеие векторов a и c" << f << "\n";
     std::cout << "Введите координаты вектора (x и y) через пробел: ";
     std::cin >> a;
-    std::cout << "Значения введенного вектора: " << a;
+    std::cout << "Значения введенного вектора: " << a;*/
+    std::cout << "Создание вектра с инициализайией значений a(1, 2.3) " << a << "Инициализация с приравниванием к а b(a) " << b;
+    BaseMathVectors<double> baseA = a.base();
+    std::cout << "Базовые вектора a: " << baseA;
+
+    b += a;
+    std::cout << "b += a; " << b;
+    b -= a;
+    std::cout << "b -= a; " << b;
+    b *= 3;
+    std::cout << "b *= 3; " << b;
+
+    return 0;
 }
