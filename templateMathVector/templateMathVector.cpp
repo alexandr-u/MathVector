64a08529956ac62b347d86e560bc2c72dd5d0035
Alexandra Uvarova <2210asd2210@gmail.com>
2024-07-09 10:18:50 +0300

    rename x to m_x, y to m_y

--- templateMathVector/templateMathVector.cpp
+++ templateMathVector/templateMathVector.cpp
@@ -1,125 +1,81 @@
 ﻿#include <iostream>
 #include <windows.h>
 #include <math.h>
-template <typename T>
-class BaseMathVectors {
-public:
-    T xVectorX;
-    T yVectorX;
-    T xVectorY;
-    T yVectorY;
-
-    friend std::istream& operator>>(std::istream& is, BaseMathVectors& item) //перегрузка метода ввода для вектора
-    {
-        is >> item.xVectorX >> item.yVectorX >> item.xVectorY >> item.yVectorY;
-        return is;
-    }
-
-    friend std::ostream& operator<<(std::ostream& os, BaseMathVectors& item) //перегрузка метода вывода для вектора
-    {
-        os << "\nКоордината x базового вектора на ось x = " << item.xVectorX << " Координата y базового вектора на ось x = " << item.yVectorX << "\nКоордината x базового вектора на ось y = " << item.xVectorY << " Координата y базового вектора на ось y = " << item.yVectorY << "\n";
-        return os;
-    }
-};
 
 template <typename T>
 
 class MathVector
 {
-private:
-    T x;
-    T y;
-
 public:
+    T m_x;
+    T m_y;
+
     MathVector() = default; //базовый конструктор
-    MathVector(T valueX, T valueY) // конструктор с параметрами
-    {
-        x = valueX;
-        y = valueY;
-    }
+    MathVector(T valueX, T valueY) : m_x(valueX), m_y(valueY) {} // список инициализации
     ~MathVector() = default; //деструктор по умолчанию
 
     MathVector(const MathVector& over) = default;
-
-    T getX() { return x; }
-    T getY() { return y; }
-    void setX(T valueX) { x = valueX; }
-    void setY(T valueY) { y = valueY; }
 
     MathVector& operator = (const MathVector& over) //перегрузка оператора =
     {
         if (this != &over)
         {
-            this->x = over.x;
-            this->y = over.y;
+            m_x = over.m_x;
+            m_y = over.m_y;
         }
         return *this;
     }
 
     MathVector operator + (const MathVector& over) //сложение векторов
     {
-        MathVector temp;
-        temp.x = this->x + over.x;
-        temp.y = this->y + over.y;
-        return temp;
+        return MathVector<T>(m_x + over.m_x, m_y + over.m_y);
     }
     MathVector operator += (const MathVector& over) { return *this = *this + over; }
 
 
     MathVector operator - (const MathVector& over) //вычитание векторов
     {
-        MathVector temp;
-        temp.x = this->x - over.x;
-        temp.y = this->y - over.y;
-        return temp;
+        return MathVector<T>(m_x - over.m_x, m_y - over.m_y);
     }
     MathVector operator -= (const MathVector& over) { return *this = *this - over; }
 
 
     MathVector operator * (const T& number) //умножение на число
     {
-        MathVector temp;
-        temp.x = this->x * number;
-        temp.y = this->y * number;
-        return temp;
+        return MathVector<T>(m_x * number, m_y * number);
     }
     MathVector operator *= (const double& number) { return *this = *this * number; }
 
     T operator * (const MathVector& over) //скалярное произведение векторов
     {
-        return (this->x * over.x + this->y * over.y);
+        return (m_x * over.m_x + m_y * over.m_y);
     }
 
     double length() //длина верктора
     {
-        return sqrt(double(pow(x, 2) + pow(y, 2)));
+        return sqrt(double(pow(m_x, 2) + pow(m_y, 2)));
+    }
+
+    std::pair<MathVector<T>, MathVector<T>> base() //разложение на базовые вектора
+    {
+        return { MathVector<T>(m_x,0),MathVector<T>(0,m_y) };
     }
 
     MathVector& normal(const MathVector& over) //нормализация вектора (приведение к единичной длине)
     {
-        this->x = double(this->x) / this->length();
-        this->y = double(this->y) / this->length();
-        return *this;
+        return MathVector<T>(double(m_x) / length(), double(m_y) / length());
     }
 
-    BaseMathVectors<T> base() //разложение на базовые вектора
-    {
-        BaseMathVectors<T> baseVectors;
-        baseVectors.xVectorX = this->x;
-        baseVectors.yVectorX = 0;
-        baseVectors.xVectorY = 0;
-        baseVectors.yVectorY = this->y;
-        return baseVectors;
-    }
+    
     friend std::istream& operator>>(std::istream& is, MathVector& item) //перегрузка метода ввода для вектора
     {
-        is >> item.x >> item.y;
+        is >> item.m_x >> item.m_y;
         return is;
     }
 
     friend std::ostream& operator<<(std::ostream& os, MathVector& item) //перегрузка метода вывода для вектора
     {
-        os << "\n x = " << item.x << " y = " << item.y << "\n";
+        os << "x = " << item.m_x << " y = " << item.m_y << "\n";
         return os;
     }
 };
@@ -139,16 +95,16 @@ int main()
     std::cout << "Введите координаты вектора (x и y) через пробел: ";
     std::cin >> a;
     std::cout << "Значения введенного вектора: " << a;*/
-    std::cout << "Создание вектра с инициализайией значений a(1, 2.3) " << a << "Инициализация с приравниванием к а b(a) " << b;
-    BaseMathVectors<double> baseA = a.base();
-    std::cout << "Базовые вектора a: " << baseA;
+    std::cout << "Создание вектра с инициализайией значений a(1, 2.3)\n " << a << "Инициализация с приравниванием к а b(a) \n" << b;
+    auto baseA = a.base();
+    std::cout << "Базовые вектора x и y вектора a: \n" << baseA.first<<baseA.second;
 
     b += a;
-    std::cout << "b += a; " << b;
+    std::cout << "\nb += a; \n" << b;
     b -= a;
-    std::cout << "b -= a; " << b;
+    std::cout << "b -= a; \n" << b;
     b *= 3;
-    std::cout << "b *= 3; " << b;
+    std::cout << "b *= 3; \n" << b;
 
     return 0;
 }
